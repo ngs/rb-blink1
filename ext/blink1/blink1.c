5d113359e4e8176086702e36b820da5791e862fb
Atsushi NAGASE <atsn.ngs@gmail.com>
2013-01-20 10:37:05 +0900

    100.00% documented

--- ext/blink1/blink1.c
+++ ext/blink1/blink1.c
@@ -1,3 +1,8 @@
+/*
+ * Blink1
+ * Ruby interface for blink(1).
+ * Wraps methods in blink1-lib.c.
+ */
 #include <stdlib.h>
 #include "ruby.h"
 #include "blink1-lib.h"
@@ -12,36 +17,61 @@ static int degamma = 1;
 
 #pragma mark - Static methods
 
+/** Return vendor ID */
 static VALUE rb_blink1_vid(VALUE self) {
   return INT2NUM(blink1_vid());
 }
 
+/** Return product ID */
 static VALUE rb_blink1_pid(VALUE self) {
   return INT2NUM(blink1_pid());
 }
 
+/** Sort cached device by path. */
 static VALUE rb_blink1_sortPaths(VALUE self) {
   blink1_sortPaths();
   return Qnil;
 }
 
+/** Sort cached device by serial id. */
 static VALUE rb_blink1_sortSerials(VALUE self) {
   blink1_sortSerials();
   return Qnil;
 }
 
+/** Get all devices by default +product_id+, +vendor_id+. */
 static VALUE rb_blink1_enumerate(VALUE self) {
   return INT2NUM(blink1_enumerate());
 }
 
+/**
+ * :call-seq:
+ *   <span class="name">enumerate_by_vid_pid</span> <span class="arguments">(vid, pid) -> integer</span>
+ *
+ * Get all matching devices by VID/PID pair.
+ *
+ * Return number of devices.
+ */
 static VALUE rb_blink1_enumerateByVidPid(VALUE self, VALUE vid, VALUE pid) {
   return INT2NUM(blink1_enumerateByVidPid(FIX2INT(vid), FIX2INT(pid)));
 }
 
+/**
+ * :call-seq:
+ *   <span class="name">cached_path</span> <span class="arguments">(index) -> string</span>
+ *
+ * Return cached device path by index.
+ */
 static VALUE rb_blink1_blink1_getCachedPath(VALUE self, VALUE i) {
   return rb_str_new2(blink1_getCachedPath(FIX2INT(i)));
 }
 
+/**
+ * :call-seq:
+ *   <span class="name">cached_serial</span> <span class="arguments">(index) -> string</span>
+ *
+ * Return cached device serial id by index.
+ */
 static VALUE rb_blink1_getCachedSerial(VALUE self, VALUE i) {
   const wchar_t *ret = blink1_getCachedSerial(FIX2INT(i));
   char dest[16] = {"\0"};
@@ -49,19 +79,29 @@ static VALUE rb_blink1_getCachedSerial(VALUE self, VALUE i) {
   return rb_str_new2(dest);
 }
 
+/**
+ * :call-seq:
+ *   <span class="name">Blink1.cached_serial</span> <span class="arguments">-> integer</span>
+ *
+ * Return cached device serial id by index.
+ */
 static VALUE rb_blink1_getCachedCount(VALUE self) {
   return INT2NUM(blink1_getCachedCount());
 }
 
+
+/* :nodoc: */
 static VALUE rb_blink1_error_msg(VALUE self, VALUE code) {
   char *msg = blink1_error_msg(FIX2INT(code));
   return msg == NULL ? Qnil : rb_str_new2(msg);
 }
 
+/** Return degamma enabled. */
 static VALUE rb_blink1_getDegammaEnabled(VALUE self) {
   return degamma == 1 ? Qtrue : Qfalse;
 }
 
+/** Set degamma enabled. */
 static VALUE rb_blink1_setDegammaEnabled(VALUE self, VALUE enabled) {
   if(RTEST(enabled)) {
     degamma = 1;
@@ -73,10 +113,12 @@ static VALUE rb_blink1_setDegammaEnabled(VALUE self, VALUE enabled) {
   return Qnil;
 }
 
+/** Return gamma corrected value for a RGB component. */
 static VALUE rb_blink1_degamma(VALUE self, VALUE i) {
   return INT2NUM(blink1_degamma(FIX2INT(i)));
 }
 
+/** Sleeps for milliseconds. */
 static VALUE rb_blink1_sleep(VALUE self, VALUE delayMillis) {
   blink1_sleep(FIX2UINT(delayMillis));
   return Qnil;
@@ -84,6 +126,7 @@ static VALUE rb_blink1_sleep(VALUE self, VALUE delayMillis) {
 
 #pragma mark - Instance methods
 
+/** :nodoc: */
 void rb_blink1_free(struct Blink1Instance *ins) {
   if(ins->opened == 1) {
     blink1_close(ins->dev);
@@ -93,18 +136,21 @@ void rb_blink1_free(struct Blink1Instance *ins) {
   ruby_xfree(ins);
 }
 
+/** :nodoc: */
 static VALUE rb_blink1_allocate(VALUE self) {
   struct Blink1Instance *ins = malloc(sizeof(struct Blink1Instance));
   ins->opened = 0;
   return Data_Wrap_Struct(self, 0, rb_blink1_free, ins);
 }
 
+/** Return the device is opened. */
 static VALUE rb_blink1_opened(VALUE self) {
   struct Blink1Instance *ins;
   Data_Get_Struct(self, struct Blink1Instance, ins);
   return ins->opened == 1 ? Qtrue : Qfalse;
 }
 
+/** Open device by default +vendor_id+, +product_id+. */
 static VALUE rb_blink1_open(VALUE self) {
   struct Blink1Instance *ins;
   Data_Get_Struct(self, struct Blink1Instance, ins);
@@ -116,6 +162,7 @@ static VALUE rb_blink1_open(VALUE self) {
   return Qfalse;
 }
 
+/** Open device by device path. */
 static VALUE rb_blink1_openByPath(VALUE self, VALUE path) {
   struct Blink1Instance *ins;
   Data_Get_Struct(self, struct Blink1Instance, ins);
@@ -127,6 +174,7 @@ static VALUE rb_blink1_openByPath(VALUE self, VALUE path) {
   return Qfalse;
 }
 
+/** Open device by serial id. */
 static VALUE rb_blink1_openBySerial(VALUE self, VALUE serial) {
   struct Blink1Instance *ins;
   Data_Get_Struct(self, struct Blink1Instance, ins);
@@ -138,6 +186,7 @@ static VALUE rb_blink1_openBySerial(VALUE self, VALUE serial) {
   return Qfalse;
 }
 
+/** Open device by id. */
 static VALUE rb_blink1_openById(VALUE self, VALUE id) {
   struct Blink1Instance *ins;
   Data_Get_Struct(self, struct Blink1Instance, ins);
@@ -149,6 +198,7 @@ static VALUE rb_blink1_openById(VALUE self, VALUE id) {
   return Qfalse;
 }
 
+/** Closes the device. */
 static VALUE rb_blink1_close(VALUE self) {
   struct Blink1Instance *ins;
   Data_Get_Struct(self, struct Blink1Instance, ins);
@@ -159,24 +209,47 @@ static VALUE rb_blink1_close(VALUE self) {
   return Qnil;
 }
 
+/** Return blink(1) version. */
 static VALUE rb_blink1_getVersion(VALUE self) {
   struct Blink1Instance *ins;
   Data_Get_Struct(self, struct Blink1Instance, ins);
   return INT2NUM(blink1_getVersion(ins->dev));
 }
 
+/**
+ * :call-seq:
+ *   <span class="name">fade_to_rgb</span> <span class="arguments">(fade_millis, r, g, b) -> integer</span>
+ *
+ * Fade LED color to RGB in +fade_millis+.
+ *
+ * Return the actual number of bytes written and -1 on error.
+ */
 static VALUE rb_blink1_fadeToRGB(VALUE self, VALUE fadeMillis, VALUE r, VALUE g, VALUE b) {
   struct Blink1Instance *ins;
   Data_Get_Struct(self, struct Blink1Instance, ins);
   return INT2NUM(blink1_fadeToRGB(ins->dev, FIX2UINT(fadeMillis), FIX2UINT(r), FIX2UINT(g), FIX2UINT(b)));
 }
 
+/**
+ * :call-seq:
+ *   <span class="name">set_rgb</span> <span class="arguments">(r, g, b) -> integer</span>
+ *
+ * Set LED color to RGB.
+ *
+ * Return the actual number of bytes written and -1 on error.
+ */
 static VALUE rb_blink1_setRGB(VALUE self, VALUE r, VALUE g, VALUE b) {
   struct Blink1Instance *ins;
   Data_Get_Struct(self, struct Blink1Instance, ins);
   return INT2NUM(blink1_setRGB(ins->dev, FIX2UINT(r), FIX2UINT(g), FIX2UINT(b)));
 }
 
+/**
+ * :call-seq:
+ *   <span class="name">eeread</span> <span class="arguments">(addr) -> integer</span>
+ *
+ * Returns an EEPROM byte.
+ */
 static VALUE rb_blink1_eeread(VALUE self, VALUE addr) {
   struct Blink1Instance *ins;
   uint8_t val = 0;
@@ -185,12 +258,21 @@ static VALUE rb_blink1_eeread(VALUE self, VALUE addr) {
   return UINT2NUM(val);
 }
 
+/**
+ * :call-seq:
+ *   <span class="name">eewrite</span> <span class="arguments">(addr, val) -> integer</span>
+ *
+ * Write an EEPROM byte.
+ *
+ * Return the actual number of bytes written and -1 on error.
+ */
 static VALUE rb_blink1_eewrite(VALUE self, VALUE addr, VALUE val) {
   struct Blink1Instance *ins;
   Data_Get_Struct(self, struct Blink1Instance, ins);
   return INT2NUM(blink1_eewrite(ins->dev, FIX2UINT(addr), FIX2UINT(val)));
 }
 
+/** :nodoc: */
 static VALUE rb_blink1_serialnumread(VALUE self) {
   struct Blink1Instance *ins;
   Data_Get_Struct(self, struct Blink1Instance, ins);
@@ -199,6 +281,7 @@ static VALUE rb_blink1_serialnumread(VALUE self) {
   return UINT2NUM(serialnum);
 }
 
+/** :nodoc: */
 static VALUE rb_blink1_serialnumwrite(VALUE self, VALUE serialnumstr) {
   struct Blink1Instance *ins;
   Data_Get_Struct(self, struct Blink1Instance, ins);
@@ -208,30 +291,70 @@ static VALUE rb_blink1_serialnumwrite(VALUE self, VALUE serialnumstr) {
   return INT2NUM(blink1_serialnumwrite(ins->dev, (uint8_t *)serialnum));
 }
 
+/**
+ * :call-seq:
+ *   <span class="name">serverdown</span> <span class="arguments">(on, millisecond) -> integer</span>
+ *
+ * Turn on/off servertickle
+ *
+ * Return the actual number of bytes written and -1 on error.
+ */
 static VALUE rb_blink1_serverdown(VALUE self, VALUE on, VALUE millis) {
   struct Blink1Instance *ins;
   Data_Get_Struct(self, struct Blink1Instance, ins);
   return INT2NUM(blink1_serverdown(ins->dev, RTEST(on) ? 1 : 0, FIX2UINT(millis)));
 }
 
+/**
+ * :call-seq:
+ *   <span class="name">play</span> <span class="arguments">(pos) -> integer</span>
+ *
+ * Start playing color sequence (at pos)
+ *
+ * Return the actual number of bytes written and -1 on error.
+ */
 static VALUE rb_blink1_play(VALUE self, VALUE pos) {
   struct Blink1Instance *ins;
   Data_Get_Struct(self, struct Blink1Instance, ins);
   return INT2NUM(blink1_play(ins->dev, 1, FIX2UINT(pos)));
 }
 
+/**
+ * :call-seq:
+ *   <span class="name">stop</span> <span class="arguments">(pos) -> integer</span>
+ *
+ * Stop playing color sequence (at pos)
+ *
+ * Return the actual number of bytes written and -1 on error.
+ */
 static VALUE rb_blink1_stop(VALUE self, VALUE pos) {
   struct Blink1Instance *ins;
   Data_Get_Struct(self, struct Blink1Instance, ins);
   return INT2NUM(blink1_play(ins->dev, 0, FIX2UINT(pos)));
 }
 
+/**
+ * :call-seq:
+ *   <span class="name">write_pattern_line</span> <span class="arguments">(fade_millis, r, g, b, pos) -> integer</span>
+ *
+ * Write pattern RGB value at pos
+ *
+ * Return the actual number of bytes written and -1 on error.
+ */
 static VALUE rb_blink1_writePatternLine(VALUE self, VALUE fadeMillis, VALUE r, VALUE g, VALUE b, VALUE pos) {
   struct Blink1Instance *ins;
   Data_Get_Struct(self, struct Blink1Instance, ins);
   return INT2NUM(blink1_writePatternLine(ins->dev, FIX2UINT(fadeMillis), FIX2UINT(r), FIX2UINT(g), FIX2UINT(b), FIX2UINT(pos)));
 }
 
+/**
+ * :call-seq:
+ *   <span class="name">read_pattern_line</span> <span class="arguments">(pos) -> hash</span>
+ *
+ * Read pattern RGB value at pos
+ *
+ * Return hash with value with key +"fade_millis"+, +"r"+, +"g"+, +"b"+
+ */
 static VALUE rb_blink1_readPatternLine(VALUE self, VALUE pos) {
   struct Blink1Instance *ins;
   Data_Get_Struct(self, struct Blink1Instance, ins);
@@ -245,12 +368,11 @@ static VALUE rb_blink1_readPatternLine(VALUE self, VALUE pos) {
   return hash;
 }
 
-/* UNSPPORT
- int blink1_write( hid_device* dev, void* buf, int len);
- int blink1_read( hid_device* dev, void* buf, int len);
- int blink1_getSerialNumber(hid_device *dev, char* buf);
- //int blink1_playPattern(hid_device *dev,,);
- */
+// UNSPPORT
+// int blink1_write( hid_device* dev, void* buf, int len);
+// int blink1_read( hid_device* dev, void* buf, int len);
+// int blink1_getSerialNumber(hid_device *dev, char* buf);
+// int blink1_playPattern(hid_device *dev,,);
 
 
 void Init_blink1() {
--- lib/blink1.rb
+++ lib/blink1.rb
@@ -1,11 +1,25 @@
 require 'blink1/blink1'
 require 'blink1/version'
 
+#
+# A Ruby interface for blink(1)[http://blink1.thingm.com/].
+#
 class Blink1
 
+  # Fade duration in millisecond.
   attr_accessor :millis
+  # Delay to next color in millisecond.
   attr_accessor :delay_millis
 
+  # :call-seq:
+  #   <span class="name">new</span> <span class="arguments">( {Fixnum} id )</span> 
+  #   <span class="name">new</span> <span class="arguments">( {Boolean} auto_open )</span>
+  #   <span class="name">new</span> <span class="arguments">( {String} serial_id )</span>
+  #   <span class="name">new</span> <span class="arguments">( :path => <em>device_path</em> )</span>
+  #   <span class="name">new</span> <span class="arguments">( :serial => <em>serial_id</em> )</span>
+  #
+  # Returns new instance of +Blink1+
+  #
   def initialize option = nil
     case option
     when Fixnum
@@ -20,11 +34,42 @@ class Blink1
       end
     when String
       open_by_serial(option)
+    else
+      open if option == true
     end
     @millis ||= 300
     @delay_millis ||= 500
   end
 
+  #
+  # :call-seq:
+  #   <span class="name">open</span> <span class="arguments">( {Fixnum} id ) { |blink1| }</span> 
+  #   <span class="name">open</span> <span class="arguments">( {Boolean} autoopen ) { |blink1| }</span>
+  #   <span class="name">open</span> <span class="arguments">( {String} serial_id ) { |blink1| }</span>
+  #   <span class="name">open</span> <span class="arguments">( :path => <em>device_path</em> ) { |blink1| }</span>
+  #   <span class="name">open</span> <span class="arguments">( :serial => <em>serial_id</em> ) { |blink1| }</span>
+  #
+  # If block given, yieds new instance of +Blink1+.
+  #
+  # If not, returns new +Blink1+
+  #
+  def self.open option = nil, &block
+    b = self.new(option)
+    b.open if option.nil?
+    if block
+      begin
+        b.instance_eval &block
+      ensure
+        b.close
+      end
+    else
+      b
+    end
+  end
+
+  #
+  # Blink with RGB value for +times+.
+  #
   def blink r, g, b, times
     times.times do
       self.fade_to_rgb(millis, r, g, b)
@@ -34,6 +79,9 @@ class Blink1
     end
   end
 
+  #
+  # Flash random color for +times+.
+  #
   def random times
     times.times do
       r = rand(0xff)
@@ -44,18 +92,30 @@ class Blink1
     end
   end
 
+  #
+  # Turn LED white.
+  #
   def on
     self.fade_to_rgb(millis, 0xff, 0xff, 0xff)
   end
 
+  #
+  # Turn LED off.
+  #
   def off
     self.fade_to_rgb(millis, 0, 0, 0)
   end
 
+  #
+  # Alias for +read_pattern_line+.
+  #
   def [] index
     self.read_pattern_line(index)
   end
 
+  #
+  # Write pattern line with hash with key +fade_millis+, +r+, +g+, +b+.
+  #
   def []= index, prop
     fade_millis = prop[:fade_millis] || prop['fade_millis']
     r = prop[:r] || prop['r']
@@ -64,6 +124,9 @@ class Blink1
     self.write_pattern_line(index, fade_millis, r, g, b)
   end
 
+  #
+  # Returns array of hash with keys +:id+, +:serial+, +:path+
+  #
   def self.list
     count = enumerate_vid_pid(vendor_id, product_id)
     i = 0
@@ -77,20 +140,6 @@ class Blink1
       i += 1
     end
     devs
-  end
-
-  def self.open option = nil, &block
-    b = self.new(option)
-    b.open if option.nil?
-    if block
-      begin
-        b.instance_eval &block
-      ensure
-        b.close
-      end
-    else
-      b
-    end
   end
 
 end
